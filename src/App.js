86ae489d4b0c312844eac3d64787fe7f91245aba
Zoltan MG <zoltanmoragarcia@gmail.com>
2022-08-06 11:38:01 -0500

    router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import { Outlet, Link } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <h1>Bookkeeper</h1>
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
+        }}
+      >
+        <Link to="/invoices">Invoices</Link> |{" "}
+        <Link to="/expenses">Expenses</Link>
+      </nav>
+      <Outlet />
     </div>
   );
 }
